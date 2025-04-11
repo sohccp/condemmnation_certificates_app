c96feb08e9f1ec2dba7f3762a4533cbf9b420004
sohccp <sohccpe@gmail.com>
2025-04-11 18:07:41 +0400

    Create auth.js

---
+++ backend/src/controllers/auth.js
@@ -0,0 +1,87 @@
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+// @desc    Register user
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { username, email, password, fullName, role, department } = req.body;
+
+  // Create user
+  const user = await User.create({
+    username,
+    email,
+    password,
+    fullName,
+    role,
+    department
+  });
+
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // Validate email & password
+  if (!username || !password) {
+    return next(new ErrorResponse('Please provide a username and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ username }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Update last login
+  user.lastLogin = Date.now();
+  await user.save({ validateBeforeSave: false });
+
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Get current logged in user
+// @route   GET /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+// @desc    Log user out / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(statusCode).json({
+    success: true,
+    token
+  });
+};
